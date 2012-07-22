ee08672214b96dfb1bf8936cad90ff11afb81f9c
Ralf Schneider <ralf@hr-interactive.com>
2012-07-22 11:53:08 +0200

    added some comments

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,6 +13,7 @@ Tx_Extbase_Utility_Extension::configurePlugin($_EXTKEY, 'Robots',
         )
         );
 
+// hook into realurl auto configuration
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration']['hriseo'] = 'EXT:hriseo/Classes/Hook/Realurl.php:Tx_Hriseo_Hook_Realurl->addHriseo';
 
 ?>
